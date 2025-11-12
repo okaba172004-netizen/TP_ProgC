c0dcd51d3033d2cb1f385291ccc4893623df4a5c
okaba172004-netizen <okaba172004@gmail.com>
2025-11-12 15:28:44 +0100

    Update conditions.c

---
+++ TP1/src/conditions.c
@@ -0,0 +1,13 @@
+Conditions et boucles avec divisibilité
+
+Écrivez un programme en C nommé conditions.c qui effectue les tâches suivantes :
+
+Utilisez une boucle for pour parcourir les nombres de 1 à 1000.
+
+Pour chaque nombre, vérifiez les conditions suivantes :
+
+Si le nombre est divisible par 5 ou par 7, ajoutez-le à une variable somme.
+Si le nombre est divisible par 11, ne l'ajoutez pas à somme et passez directement au nombre suivant (utilisez continue).
+Si la somme dépasse 5000 à tout moment, arrêtez immédiatement la boucle (utilisez break).
+
+Affichez la somme finale après avoir terminé la boucle ou après avoir interrompu le calcul si la somme a dépassé 5000.
